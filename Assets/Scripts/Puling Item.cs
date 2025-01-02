b934ccdb13f0fd5e70a5e6ab5fbd58352c7e8822
NourTantawy <nour.amr.tantawy@gmail.com>
2025-01-02 16:57:27 +0200

    Thor's hammer

--- Assets/Scripts/Puling Item.cs
+++ Assets/Scripts/Puling Item.cs
@@ -1,31 +1,49 @@
-using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PullingItem : MonoBehaviour
 {
     [SerializeField] private float ForceMagnitude = 30f;
-    [SerializeField] private float range = 5f;
+    [SerializeField] private float range = 0f;
 
+    // Logic
     private bool isDragging = false;
-    
+
+    // Ray Casting
+    private RaycastHit hitInfo;
+
     private void Update() 
     {
-        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, 1000f)) 
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.TransformDirection(Vector3.forward), out hitInfo, 1000f)) 
         {
             float distance = Vector3.Distance(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), hitInfo.point);
-            Rigidbody rigidbody = hitInfo.collider.GetComponent<Rigidbody>();
+            
 
             if (hitInfo.collider.CompareTag("Pullable") && Input.GetKey(KeyCode.M) && distance < range)
             {
-                rigidbody.transform.SetParent(this.transform, true);
-                //rigidbody.transform.SetParent(this.transform, false);
-                rigidbody.transform.position = Vector3.zero; 
+                isDragging = true;
             }
             else 
             {
-                rigidbody.transform.SetParent(null);
+                isDragging = false;
             }
+
+            
         }   
+        
+        Rigidbody rigidbody = hitInfo.collider.GetComponent<Rigidbody>();
+
+        if (isDragging)
+            {
+                Vector3 boxDirection = hitInfo.transform.position;
+                boxDirection.y = hitInfo.transform.position.y;
+                
+                Vector3 forceDirection = this.transform.position - hitInfo.transform.position;
+
+                forceDirection.y = 0;
+                forceDirection.Normalize();
+
+                rigidbody.AddForce(forceDirection * ForceMagnitude, ForceMode.Impulse);
+            }
     }
 }
