761f5fe9305bc5cdc425bc2c2cbffa4427e77ade
ON4Saleh <116005799+ON4Saleh@users.noreply.github.com>
2025-01-02 18:12:25 +0200

    Update Final Merge

---
+++ Assets/Scripts/Puling Item.cs
@@ -0,0 +1,49 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PullingItem : MonoBehaviour
+{
+    [SerializeField] private float ForceMagnitude = 30f;
+    [SerializeField] private float range = 0f;
+
+    // Logic
+    private bool isDragging = false;
+
+    // Ray Casting
+    private RaycastHit hitInfo;
+
+    private void Update() 
+    {
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.TransformDirection(Vector3.forward), out hitInfo, 1000f)) 
+        {
+            float distance = Vector3.Distance(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), hitInfo.point);
+            
+
+            if (hitInfo.collider.CompareTag("Pullable") && Input.GetKey(KeyCode.F) && distance < range)
+            {
+                isDragging = true;
+            }
+            else 
+            {
+                isDragging = false;
+            }
+
+            
+        }   
+        
+        Rigidbody rigidbody = hitInfo.collider.GetComponent<Rigidbody>();
+
+        if (isDragging)
+            {
+                Vector3 boxDirection = hitInfo.transform.position;
+                boxDirection.y = hitInfo.transform.position.y;
+                
+                Vector3 forceDirection = this.transform.position - hitInfo.transform.position;
+
+                forceDirection.y = 0;
+                forceDirection.Normalize();
+
+                rigidbody.AddForce(forceDirection * ForceMagnitude, ForceMode.Impulse);
+            }
+    }
+}
