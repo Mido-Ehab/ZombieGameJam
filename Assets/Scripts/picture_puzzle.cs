e3937247e5371fe21c872739800d2e3d41650c3f
pofox <66141963+pofox@users.noreply.github.com>
2025-01-03 16:29:56 +0200

    puzzle completed

--- Assets/Scripts/picture_puzzle.cs
+++ Assets/Scripts/picture_puzzle.cs
@@ -1,17 +1,26 @@
+using UnityEditor;
 using UnityEngine;
 
 public class picture_puzzle : MonoBehaviour
 {
-    
+    public GameObject box1;
+    public GameObject box4;
+    box1 b1;
+    box4 b4;
+    public bool iswin = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        b1 = box1.GetComponent<box1>();
+        b4 = box4.GetComponent<box4>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (b1.right && b1.down && b4.left && b4.up)
+        {
+            iswin = true;
+        }
     }
 }
