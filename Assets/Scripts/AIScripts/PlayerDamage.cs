d1a31a15fea92a005ad9701b72452df5491525ef
ON4Saleh <116005799+ON4Saleh@users.noreply.github.com>
2025-01-04 17:56:04 +0200

    collectable, puzzle ,and door done

--- Assets/Scripts/AIScripts/PlayerDamage.cs
+++ Assets/Scripts/AIScripts/PlayerDamage.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerHealthReducer : MonoBehaviour
+public class PlayerDamage : MonoBehaviour
 {
     private int Health = 3;
 
